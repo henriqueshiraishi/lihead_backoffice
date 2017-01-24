a9e56f2665fde3081e0701a27e21a6ca71ef2dcb
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2017-01-24 11:07:47 -0200

    Commit 2
    Integrando a aplicação escamboaap (curso Udemy) e uso de AJAX no item
    Produtos.

--- app/controllers/backoffice_controller.rb
+++
@@ -1,9 +0,0 @@
-class BackofficeController < ApplicationController
-  before_action :authenticate_admin!
-  layout 'backoffice'
-
-  def pundit_user
-    current_admin
-  end
-
-end
--- db/migrate/20170118143528_add_role_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleToAdmin < ActiveRecord::Migration
-  def change
-    add_column :admins, :role, :integer, null: false, default: 1
-  end
-end
